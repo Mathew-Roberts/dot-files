3cce08bbb5a6b2edf23fd71aa8f0b26129ef6d3d
mat roberts <mathewjamesroberts99@gmail.com>
2024-08-31 20:54:39 +0100

    adding .config with nvim in it

---
+++ .config/lua/vim/vim-keymaps.lua
@@ -0,0 +1,56 @@
+-- Setting keymaps
+
+-- Exiting Normal Mode
+vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Exiting Insert mode' })
+
+--Get rid of highlighting after a search
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+local set_var = function()
+  print 'Setting PYTHONPATH and VIRTUAL_ENV for debugging'
+  local pypath = os.getenv 'GUIX_PYTHONPATH' .. ':' .. vim.fn.getcwd() .. '/src'
+  vim.env.PYTHONPATH = pypath
+  vim.env.VIRTUAL_ENV = os.getenv 'GUIX_ENVIRONMENT'
+end
+
+local unset_var = function()
+  print 'Unsetting PYTHONPATH and VIRTUAL_ENV'
+  vim.env.PYTHONPATH = vim.NIL
+  vim.env.VIRTUAL_ENV = vim.NIL
+end
+
+vim.keymap.set('n', '<leader>!', set_var, { desc = 'Setting PYTHONPATH and VIRTUAL_ENV' })
+vim.keymap.set('n', '<leader>@', unset_var, { desc = 'Unsetting PYTHONPATH and VIRTUAL_ENV' })
+
+-- vim: ts=2 sts=2 sw=2 et

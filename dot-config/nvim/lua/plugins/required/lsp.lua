e02704dbae6a8981efb1aa00f3c4ae146d085478
mat roberts <mathewjamesroberts99@gmail.com>
2024-12-22 21:58:01 +0000

    removing some excess comments from kickstart

--- dot-config/nvim/lua/plugins/required/lsp.lua
+++ dot-config/nvim/lua/plugins/required/lsp.lua
@@ -10,7 +10,7 @@ return { -- LSP Configuration & Plugins
   },
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
       callback = function(event)
         -- Setting lsp key maps
         local map = function(keys, func, desc)
@@ -28,14 +28,10 @@ return { -- LSP Configuration & Plugins
         map('K', vim.lsp.buf.hover, 'Hover Documentation')
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        -- Highlight current word on cursor
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.server_capabilities.documentHighlightProvider then
-          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+          local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
             group = highlight_augroup,
@@ -49,10 +45,10 @@ return { -- LSP Configuration & Plugins
           })
 
           vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
             callback = function(event2)
               vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
             end,
           })
         end
@@ -62,10 +58,10 @@ return { -- LSP Configuration & Plugins
     -- Ensuring that the source of diagnostics is always showing
     vim.diagnostic.config {
       virtual_text = {
-        source = 'always',
+        source = 'if_many',
       },
       float = {
-        source = 'always',
+        source = 'if_many',
       },
     }
 
@@ -100,7 +96,7 @@ return { -- LSP Configuration & Plugins
     -- Ensuring all lsp/formatting/linters are installed
     require('mason').setup()
     local ensure_installed = vim.tbl_keys(opts.servers)
-    local linters = { 'ruff', 'mypy', 'markdownlint', 'jsonlint' }
+    local linters = { 'mypy', 'markdownlint', 'jsonlint' }
     local formatters = { 'stylua', 'fixjson', 'clang-format', 'rustfmt' }
     vim.list_extend(ensure_installed, linters)
     vim.list_extend(ensure_installed, formatters)

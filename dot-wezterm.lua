2e503af5ec3840f581ad953e7f04f3ffbc45b353
mat roberts <mathewjamesroberts99@gmail.com>
2024-12-25 15:34:19 +0000

    adding transparency

--- dot-wezterm.lua
+++ dot-wezterm.lua
@@ -3,6 +3,11 @@ local wezterm = require("wezterm")
 
 -- Hold the configuration
 local config = wezterm.config_builder()
+
 config.font_size = 14
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
 
 return config

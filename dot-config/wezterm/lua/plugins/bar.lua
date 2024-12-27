81a6c2d785049ac4f239902c6c19c51880bc686d
mat roberts <mathewjamesroberts99@gmail.com>
2024-12-27 19:56:27 +0000

    further modularisation and adding bar.lua wezterm plugin

---
+++ dot-config/wezterm/lua/plugins/bar.lua
@@ -0,0 +1,26 @@
+local wezterm = require 'wezterm'
+local bar = wezterm.plugin.require 'https://github.com/adriankarlen/bar.wezterm'
+local M = {}
+
+function M.setup(config)
+  -- example enable spotify module
+  local bar_config = {
+    modules = {
+      spotify = {
+        enabled = false,
+      },
+      username = {
+        enabled = false,
+      },
+      hostname = {
+        enabled = false,
+      },
+      cwd = {
+        enabled = true,
+      },
+    },
+  }
+  bar.apply_to_config(config, bar_config)
+end
+
+return M
--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -4,16 +4,14 @@ local wezterm = require 'wezterm'
 -- Hold the configuration
 local config = wezterm.config_builder()
 
-wezterm.on('update-right-status', function(window, pane)
-  window:set_right_status(window:active_workspace())
-end)
-
 local opts = require 'lua.opts'
 local keys = require 'lua.keymaps'
-local move = require 'lua.move'
+local move = require 'lua.plugins.move'
+local bar = require 'lua.plugins.bar'
 
 opts.setup(config)
 keys.setup(config)
 move.setup(config)
+bar.setup(config)
 
 return config
